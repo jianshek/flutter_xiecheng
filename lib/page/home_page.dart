4479d4b794b83e9181c8feaad3b5df7ca6554a7a
flyingish <948698823@qq.com>
2020-10-20 09:19:23 +0800

    添加轮播图功能

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatefulWidget{
   @override
@@ -8,8 +9,48 @@ class HomePage extends StatefulWidget{
 class __HomePageState extends State<HomePage>{
   @override
   Widget build(BuildContext context) {
+
+    List _bannerList = [
+      'http://via.placeholder.com/350x150',
+      'http://via.placeholder.com/350x150',
+      'http://via.placeholder.com/350x150'
+    ];
+
     return Scaffold(
-      body: Text("首页"),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              height: 160,
+              child: BannerWidget(bannerList: _bannerList),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+}
+
+class BannerWidget extends StatelessWidget{
+
+  final List _bannerList;
+
+  BannerWidget({Key key,@required List bannerList}) : _bannerList = bannerList,
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 240,
+      child: Swiper(
+        itemCount: _bannerList.length,
+        autoplay: true,
+        itemBuilder: (BuildContext context,int index){
+          return Image.network(_bannerList[index],fit: BoxFit.fill);
+        },
+        pagination: new SwiperPagination()
+      ),
     );
   }
 
